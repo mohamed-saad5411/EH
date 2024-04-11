83eced33ee534f9c1fec90755150516b6c535c9e
mohamed saad <124407370+mohamed-saad5411@users.noreply.github.com>
2024-04-12 01:50:38 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,62 @@
+
+// singers
+$("#singer0").click(function () {
+    $("#text0").slideToggle(600)
+})
+
+$("#singer1").click(function () {
+    $("#text1").slideToggle(600)
+})
+
+$("#singer2").click(function () {
+    $("#text2").slideToggle(600)
+})
+
+$("#singer3").click(function () {
+    $("#text3").slideToggle(600)
+})
+
+$("#singer4").click(function () {
+    $("#text4").slideToggle(600)
+})
+$("#singer5").click(function () {
+    $("#text5").slideToggle(600)
+})
+
+$("#singer6").click(function () {
+    $("#text6").slideToggle(600)
+})
+
+$("#singer7").click(function () {
+    $("#text7").slideToggle(600)
+})
+
+$("#singer8").click(function () {
+    $("#text8").slideToggle(600)
+})
+$("#singer20").click(function () {
+    $("#text20").slideToggle(600)
+})
+
+
+// let arabic = document.getElementsByClassName(".arabic")
+// let english = document.getElementsByClassName(".english")
+
+$("#arabic").click(function () {
+    $(".arabic").css("display","block")
+    $(".english").css("display","none")
+    $("#english").css("display","block")
+    $("#arabic").css("display","none")
+})
+
+$("#english").click(function () {
+    $(".arabic").css("display","none")
+    $(".english").css("display","block")
+    $("#english").css("display","none")
+    $("#arabic").css("display","block")
+})
+
+
+
+
+
